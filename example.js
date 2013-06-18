0621562600aea47f10689de35169f0bdab525744
Mikael Brassman <mishakun@gmail.com>
2013-06-18 14:55:18 +0200

    Added originelle (hbg) in example

--- example.js
+++ example.js
@@ -4,6 +4,7 @@ var S = require('string');
 
 var i, place, lunchArr = [
 	{
+		enabled: true,
 		url: 'http://www.kgrand.se',
 		header: 'Kgränd',
 		scrape: function($) {
@@ -17,18 +18,32 @@ var i, place, lunchArr = [
 		}
 	},
 	{
+		enabled: true,
 		url: 'http://www.inom-matbar.se/lunch.php',
 		header: 'Inom - Mat & Bar',
 		scrape: function($) {
 			var allText = '';
 			$('#column1').each(function (){
 				var text = this.text();
-				text = S(text).replaceAll(/([A-Z][\wåäö]{5,6}\s[0-9]{2}\/[0-9])/g, '\n');
 				allText += text;
 			});
 			return allText;
 		}
-	}
+	},
+	{
+		enabled: true,
+		url: 'http://originelle.nu/pages/template/lunchmenu.php',
+		header: 'Originelle',
+		scrape: function($, body) {
+			var allText = '';
+			var arr = $('table table span').text().split('\n');
+			var i;
+			for(i = 0; i < arr.length; i++) {
+				if(!S(arr[i]).isEmpty()) allText += arr[i] + '\n';
+			}
+			return allText;
+		}
+	}	
 ], requestPlace = function(place) {
 	request(place.url, function(err, resp, body) {
 		if (err) {
@@ -38,10 +53,10 @@ var i, place, lunchArr = [
 
 		console.log(place.header);
 		console.log(S('-').repeat(place.header.length).s);
-		console.log(place.scrape($));
+		console.log(place.scrape.call(body, $));
 	});	
 };
 
 for (i = 0; i < lunchArr.length; i++) {
-	requestPlace(lunchArr[i]);
+	if (lunchArr[i].enabled) requestPlace(lunchArr[i]);
 }
